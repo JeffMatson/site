bf0f9c39297bc849d6465f06401d37dc46241ce5
JeffMatson <JeffMatson@users.noreply.github.com>
2024-09-24 17:36:56 -0400

    Site is WIP in rebuild. Upgrading for now to get a post out.... YOLO

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,50 +1,53 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 /**
  * Single pages collection.
  */
 const singleCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        description: z.string().default(''),
-        author: z.string().default('jeffmatson'),
-    }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string().default(""),
+    author: z.string().default("jeffmatson"),
+  }),
 });
 
 /**
  * Blog posts collection.
  */
 const blogCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        description: z.string().default(''),
-        comments: z.boolean().default(true),
-        date: z.string().transform(str => new Date(str)).default(new Date().toISOString()),
-        author: z.string().default('jeffmatson'),
-        image: z.string().optional(),
-    }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string().default(""),
+    comments: z.boolean().default(true),
+    date: z
+      .string()
+      .transform((str) => new Date(str))
+      .default(new Date().toISOString()),
+    author: z.string().default("jeffmatson"),
+    image: z.string().optional(),
+  }),
 });
 
 /**
  * Authors collection
  */
 const authorsCollection = defineCollection({
-    schema: z.object({
-        name: z.string(),
-        description: z.string().default(''),
-        firstName: z.string(),
-        lastName: z.string(),
-        knowsAbout: z.array(z.string()).optional(),
-        url: z.string().optional(),
-        sameAs: z.array(z.string()).optional().default([]),
-    }),
+  schema: z.object({
+    name: z.string(),
+    description: z.string().default(""),
+    firstName: z.string(),
+    lastName: z.string(),
+    knowsAbout: z.array(z.string()).optional(),
+    url: z.string().optional(),
+    sameAs: z.array(z.string()).optional().default([]),
+  }),
 });
 
 /**
  * Collection tags. Maps to `/src/content/<collection>`.
  */
 export const collections = {
-    'single': singleCollection,
-    'blog': blogCollection,
-    'authors': authorsCollection,
+  single: singleCollection,
+  blog: blogCollection,
+  authors: authorsCollection,
 };
